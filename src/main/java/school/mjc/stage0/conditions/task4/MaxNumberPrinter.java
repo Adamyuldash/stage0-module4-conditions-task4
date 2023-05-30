ab6b882451a31d6df42252bf2019022bf4a4173c
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-05-30 05:47:13 -0700

    ''

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+
+
+        int num1;
+        int greatestNumber = first > second ? first : second;
+        greatestNumber = greatestNumber > third ? greatestNumber : third;
+
+        System.out.println(greatestNumber);
     }
 }
