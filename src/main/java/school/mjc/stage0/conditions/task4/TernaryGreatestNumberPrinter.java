a51d296b42ea62573c19c8f5453ab0eef152f36c
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-05-30 05:47:13 -0700

    ''

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+
+
+        int greatestNumber = Math.max(first, second);
+
+
+        System.out.println(greatestNumber);
     }
-}
+
+            }
+
--- src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryMaxNumberPrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryMaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int greatestNumber = first > second ? first : second;
+        greatestNumber = greatestNumber > third ? greatestNumber : third;
+
+        System.out.println(greatestNumber);
     }
 }
